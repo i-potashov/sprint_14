97427a769d44eb70c976224de16b68f97d17eea8
i-potashov <i.ptshv@gmail.com>
2020-03-06 00:22:02 +0300

     Add authorization

--- routes/cards.js
+++ routes/cards.js
@@ -2,7 +2,7 @@ const cards = require('express').Router();
 const { getCards, createCard, deleteCard } = require('../controllers/cards');
 const { cardRequestCheck } = require('../modules/validations');
 
-cards.get('/cards', cardRequestCheck, getCards);
+cards.get('/cards', getCards);
 cards.post('/cards', cardRequestCheck, createCard);
 cards.delete('/cards/:cardId', deleteCard);
 
